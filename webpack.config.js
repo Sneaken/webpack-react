d149050abe2ed29afcd2180608956fb3103bdf06
Bin Jiang <bin.jiang@geneegroup.com>
2021-04-20 13:33:05 +0800

    build: 安装 html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,15 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 function resolve (dir) {
   return path.resolve(__dirname, dir)
 }
 
-module.exports = {
+const config = {
   // 指定构建环境
   mode: "development",
   // 入口
-  entry: {
-    app: "./src/index"
-  },
+  entry: "./src/index.js",
   // 出口
   output: {
     path: resolve("dist"),
@@ -20,9 +19,14 @@ module.exports = {
   // 模块
   module: {},
   // 插件
-  plugins: [],
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: resolve('dist/index.html'), // html模板的生成路径
+      template: resolve('public/index.html'),// html模板
+    })
+  ],
   // // 开发环境本地启动的服务配置
-  // devServer: {
-  //
-  // }
+  // devServer: {}
 }
+
+module.exports = config
