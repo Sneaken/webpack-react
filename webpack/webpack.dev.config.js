9a3de936da6317b7a0b3969c568cb0ee282321a6
Sneaken <924393527@qq.com>
2021-04-21 19:09:01 +0800

    build: devServer proxy

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,20 +1,36 @@
-const {merge} = require('webpack-merge')
-const baseWebpackConfig = require("./webpack.base.config")
-const utils = require("./utils")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { merge } = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.base.config');
+const utils = require('./utils');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(baseWebpackConfig, {
   // 指定构建环境
-  mode: "development",
+  mode: 'development',
   // 插件
   plugins: [
     new HtmlWebpackPlugin({
       title: 'webpack-react',
       template: utils.resolve('./../public/index.html'), // html模板
-    })
+    }),
   ],
   // 开发环境本地启动的服务配置
   devServer: {
-    open: true
-  }
-})
+    open: true,
+    // 接口转发
+    proxy: {
+      '/api': {
+        target: 'http://www.baidu.com', // 访问/api/xxx 实际会转发到 http://www.baidu.com/api/xxx
+      },
+      '/api2': {
+        target: 'http://www.baidu.com',
+        pathRewrite: {
+          '^/api': '', // 访问/api2/xxx 实际会转发到 http://www.baidu.com/xxx
+        },
+      },
+      '/api3': {
+        target: 'https://www.baidu.com', // 访问/api3/xxx 实际会转发到 http://www.baidu.com/api3/xxx
+        secure: false, // 防止转发失败 （默认情况下，将不接受在 HTTPS 上运行且证书无效的后端服务器。）
+      },
+    },
+  },
+});
